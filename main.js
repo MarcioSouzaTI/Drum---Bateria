f7e33817a97328ca19f5e7cc6c7b9231459cd283
Marcio Souza <contadm305@gmail.com>
2023-09-15 18:09:57 -0300

    ajustando entra do evento

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let teclasPressionadas = {};
+
 function tocaSom(seletorAudio) {
   const elemento = document.querySelector(seletorAudio);
 
@@ -19,28 +21,39 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
   tecla.onclick = function () {
     tocaSom(idAudio);
-  }
+  };
 }
 
 // Mapeamento de teclas para botões
 const teclaParaBotao = {
-  "KeyI": "#som_tecla_tim", // Tecla "i" para o botão .tecla_tim
-  "KeyY": "#som_tecla_pom", // Tecla "y" para o botão .tecla_pom
-  "KeyU": "#som_tecla_psh", // Tecla "u" para o botão .tecla_psh
-  "KeyH": "#som_tecla_puff", // Tecla "h" para o botão .tecla_puff 
-  "KeyJ": "#som_tecla_clap", // Tecla "j" para o botão .tecla_clap
-  "KeyK": "#som_tecla_tic", // Tecla "k" para o botão .tecla_tic
-  "KeyA": "#som_tecla_tom", // Tecla "a" para o botão .tecla_tom
-  "KeyS": "#som_tecla_splash", // Tecla "s" para o botão .tecla_splash
-  "KeyD": "#som_tecla_toim", // Tecla "d" para o botão .tecla_toim
-  // Adicione mais mapeamentos conforme necessário
+  "KeyI": "#som_tecla_tim",
+  "KeyY": "#som_tecla_pom",
+  "KeyU": "#som_tecla_psh",
+  "KeyH": "#som_tecla_puff",
+  "KeyJ": "#som_tecla_clap",
+  "KeyK": "#som_tecla_tic",
+  "KeyA": "#som_tecla_tom",
+  "KeyS": "#som_tecla_splash",
+  "KeyD": "#som_tecla_toim",
 };
 
 // Adicione um ouvinte de evento de teclado ao documento
 document.addEventListener("keydown", function(event) {
   const teclaPressionada = event.code;
   const seletorAudio = teclaParaBotao[teclaPressionada];
-  if (seletorAudio) {
-    tocaSom(`#${seletorAudio.substring(1)}`); // Remove o ponto do seletor
+
+  if (seletorAudio && !teclasPressionadas[teclaPressionada]) {
+    tocaSom(seletorAudio);
+    teclasPressionadas[teclaPressionada] = true;
   }
 });
+
+// Adicione um ouvinte de evento para quando a tecla é liberada
+document.addEventListener("keyup", function(event) {
+  const teclaPressionada = event.code;
+  if (teclasPressionadas[teclaPressionada]) {
+    teclasPressionadas[teclaPressionada] = false;
+  }
+});
+
+
